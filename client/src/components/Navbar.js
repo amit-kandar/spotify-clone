a9d3238f0762e5e4b3fd23cf583088fb870904ad
Amit Kandar <kandaramit2001@gmail.com>
2022-08-02 02:22:18 +0530

    Update logo Name

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">MyNotebook</Link>
+                    <Link className="navbar-brand" to="/">SpotifyClone</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
